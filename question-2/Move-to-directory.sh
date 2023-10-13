3795247342821c755f07fd65861d558aa13b73dd
Mgrace1 <g.munezero1@alustudent.com>
2023-10-13 08:26:52 -0700

    message

--- question-2/Move-to-directory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create the directory name
-mkdir -p "Negpod-11_q1"
-
-# Move the files to the target directory
-mv main.sh "students-list_0923.txt" select-emails.sh "student-emails.txt" "Negpod_11-q1"/
-
-echo "Files moved to Negpod_11-q1."
